2aa580f1cb59fb97b96c08e693fe930ac77be29b
Ricardo P Oliveira <ricardooliveira.pl216@academico.ifsul.edu.br>
2023-08-25 16:08:51 -0300

    add: classe Aluno

--- Avaliacao_Objetivo_01/src/model/Aluno.java
+++ Avaliacao_Objetivo_01/src/model/Aluno.java
@@ -9,4 +9,70 @@ public class Aluno {
     private String sobrenome;
     private String email;
 
+    //Construtores
+    public Aluno() {
+    }
+
+    public Aluno(int id, int cpf, String nome, String sobrenome, String email) {
+        this.id = id;
+        this.cpf = cpf;
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.email = email;
+    }
+
+    //Getters and Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    //toString
+
+    @Override
+    public String toString() {
+        return "Aluno {" +
+                "id=" + id +
+                ", cpf=" + cpf +
+                ", nome='" + nome + '\'' +
+                ", sobrenome='" + sobrenome + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
