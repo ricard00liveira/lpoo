56911091b3b2da2355db8309cf08126601dc289c
Ricardo P Oliveira <ricardooliveira.pl216@academico.ifsul.edu.br>
2023-08-25 16:05:39 -0300

    first commit

---
+++ Avaliacao_Objetivo_01/src/model/Aluno.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Aluno {
+
+    //Atributos
+    private int id;
+    private int cpf;
+    private String nome;
+    private String sobrenome;
+    private String email;
+
+}
